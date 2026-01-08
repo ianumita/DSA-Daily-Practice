38d413abca352b99a786a060c29014c6f370a825
Anumita Bose <166149263+ianumita@users.noreply.github.com>
2026-01-08 16:42:30 +0530

    Create contains-duplicate.py

---
+++ Practice/Arrays/contains-duplicate.py
@@ -0,0 +1,13 @@
+
+# Time Complixity: 
+# Space Complixity: 
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        l = len(nums)
+        for i in nums:
+            if i in seen:
+                return True
+            seen.add(i)
+        return False
